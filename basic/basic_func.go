9622e45cca38adeb4eee8c661c74ee254ea9d3a1
RohamN0 <rohamnodoust.official@gmail.com>
2025-12-30 19:44:58 +0330

    feat: add tokenizer support

--- basic/basic_func.go
+++ basic/basic_func.go
@@ -5,100 +5,59 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+
+	"shell/tokenizer"
 )
 
 func Echo(command string) {
-	_, text, _ := strings.Cut(command, "echo ")
-	fmt.Println(text)
+	command_splited := tokenizer.Tokenize(command)[1:]
+	for i, text := range(command_splited) {
+		command_splited[i] = tokenizer.EchoTokenize(text)
+	}
+	command = strings.Join(command_splited, " ")
+
+	fmt.Println(command)
 }
 
 func Type(command string) {
-	if command == "type" {
-		fmt.Println("type is a shell builtin")
-	} else {
-		removed_from_command := strings.Join(strings.Split(command, "type ")[1:], " ")
-		valid_commands := [] string{"echo", "exit", "type", "pwd", "cd"}
+	commands := tokenizer.Tokenize(command)[1:]
+	valid_commands := [] string{"echo", "exit", "type", "pwd", "cd"}
 
-		for {
-			// using trim and split to remove all the spaces and get the actual text each iteration
-			removed_from_command = strings.Trim(removed_from_command, " ")
-			removed_from_command_splited := strings.Split(removed_from_command, " ")
-			second_command := removed_from_command_splited[0]
-
-			// break statement
-			if second_command == "" {
+	for _, command := range(commands) {
+		flag := false
+		for _, valid_command := range(valid_commands) {
+			// cheking of the text is a valid command or not
+			if valid_command == command {
+				fmt.Println(command + " is a shell builtin")
+				flag = true
 				break
 			}
-			
-			flag := false
-			for _, valid_command := range(valid_commands) {
-				// cheking of the text is a valid command or not
-				if valid_command == second_command {
-					fmt.Println(second_command + " is a shell builtin")
-					flag = true
-					break
-				}
-			}
+		}
 
-			if !flag {
-				path, err := exec.LookPath(second_command)
-				if err != nil {
-					fmt.Println(second_command + ": not found")
-				} else {
-					fmt.Println(second_command + " is " + path)
-				}
+		if !flag {
+			path, err := exec.LookPath(command)
+			if err != nil {
+				fmt.Println(command + ": not found")
+			} else {
+				fmt.Println(command + " is " + path)
 			}
-			
-			removed_from_command = strings.Join(removed_from_command_splited[1:], " ")
 		}
 	}
 }
 
 func RunExternalProgram(command string) {
-	// checking if there is only one command with no args
-	_, err := exec.LookPath(command)
-	if err == nil {
-		cmd := exec.Command(command)
+    args := tokenizer.Tokenize(command)
+    prog := args[0]
+
+	_, err := exec.LookPath(prog)
+    if err != nil {
+		fmt.Println(prog + ": command not found")
+    } else {
+		cmd := exec.Command(prog, args[1:]...)
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
-		if err := cmd.Run(); err != nil {
-    		fmt.Fprintf(os.Stderr, "%v\n", err)
-		}
 
-	} else {
-		actual_command := strings.Split(command, " ")[0]
-
-		_, err := exec.LookPath(actual_command)
-		if err != nil {
-			fmt.Println(actual_command + ": command not found")
-		} else {
-			args := [] string{actual_command}
-			removed_from_command := strings.Join(strings.Split(command, " ")[1:], " ")
-
-			for {
-				// using trim and split to remove all the spaces and get the actual text each iteration
-				removed_from_command = strings.Trim(removed_from_command, " ")
-				removed_from_command_splited := strings.Split(removed_from_command, " ")
-				arg := removed_from_command_splited[0]
-				
-				// break statement
-				if arg == "" {
-					break
-				}
-				// appending all the args
-				args = append(args, arg)
-				
-				removed_from_command = strings.Join(removed_from_command_splited[1:], " ")
-			}
-
-			cmd := exec.Command(args[0], args[1:]...)
-			cmd.Stdin = os.Stdin
-			cmd.Stdout = os.Stdout
-			cmd.Stderr = os.Stderr
-			if err := cmd.Run(); err != nil {
-    			fmt.Fprintf(os.Stderr, "%v\n", err)
-			}
-		}
+		cmd.Run()
 	}
 }
---
+++ tokenizer/tokenizer.go
@@ -0,0 +1,72 @@
+package tokenizer
+
+import (
+	"strings"
+)
+
+func Tokenize(line string) ([]string) {
+    var args [] string
+    var current strings.Builder
+    inSingle := false
+    inDouble := false
+    escaped := false
+
+    for i := 0; i < len(line); i++ {
+        c := line[i]
+
+        if escaped {
+            current.WriteByte(c)
+            escaped = false
+            continue
+        }
+
+        switch c {
+        case '\'' :
+            if !inDouble {
+                inSingle = !inSingle
+                continue
+            }
+            current.WriteByte(c)
+
+        case '"' :
+            if !inSingle {
+                inDouble = !inDouble
+                continue
+            }
+            current.WriteByte(c)
+
+        case ' ', '\t', '\n' :
+            if !inSingle && !inDouble {
+                if current.Len() > 0 {
+                    args = append(args, current.String())
+                    current.Reset()
+                }
+                continue
+            }
+            current.WriteByte(c)
+
+        default :
+            current.WriteByte(c)
+        }
+    }
+
+    if current.Len() > 0 {
+        args = append(args, current.String())
+    }
+
+    if inSingle || inDouble {
+        return nil
+    }
+
+    return args
+}
+
+func EchoTokenize(text string) string {
+	if splited := strings.Split(text, ""); splited[0] == "'" && splited[len(splited) - 1] == "'" {
+		text = strings.ReplaceAll(text, "'", "")
+	}
+	text_splited := strings.Fields(text)
+	text = strings.Join(text_splited, " ")
+
+	return text
+}
